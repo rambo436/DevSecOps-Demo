b4dc4d2f2e2b31a2290cea45ca361ad20ba9bd16
Kai Prout <kai@moonrake.it>
2019-10-01 19:50:32 -0400

    Implementing eks management module.

--- terraform/aws/development/eks-cluster.tf
+++ terraform/aws/development/eks-cluster.tf
@@ -72,4 +72,18 @@ resource "aws_security_group" "moonrake-demo-cluster-ingress-node-https" {
   }"
 }
 
-# Creating
+# Creating EKS cluster.
+resource "aws_eks_cluster" "moonake_demo" {
+  name     = "${var.cluster-name}"
+  role_arn = "${aws_iam_role.demo-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.moonrake-demo-cluster.id}"]
+    subnet_ids         = "${aws_subnet.moonrake_demo[*].id}"
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.moonrake-demo-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.moonrake-demo-cluster-AmazonEKSServicePolicy",
+  ]
+}
