0c79134a6fb210e4e0d930b5151c664c18cabdb8
Kai Prout <kai@moonrake.it>
2019-10-01 18:38:23 -0400

    Adding internet gateway, fixing naming conventions.

--- terraform/aws/development/vpc.tf
+++ terraform/aws/development/vpc.tf
@@ -1,19 +1,19 @@
 # Configuring AWS VPC Resources
 
 # Adding VPC
-resource "aws_vpc" "moonrake-demo" {
+resource "aws_vpc" "moonrake_demo" {
   cidr_block = "10.0.0.0/16"
 
   tags = "${
     map(
-      "Name", "moonrake-eks-demo",
+      "Name", "moonrake-eks-demo-vpc",
       "kubernetes.io/cluster/${var.cluster-name}", "shared",
     )
   }"
 }
 
 # Adding Subnets to VPC
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "moonrake_demo" {
   count = 3
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
@@ -22,8 +22,17 @@ resource "aws_subnet" "demo" {
 
   tags = "${
     map(
-      "Name", "terraform-eks-demo-node",
+      "Name", "moonrake-eks-demo-subnet",
       "kubernetes.io/cluster/${var.cluster-name}", "shared",
     )
   }"
+}
+
+# Adding AWS Internet Gateway to VPC
+resource "aws_internet_gateway" "moonrake_demo" {
+  vpc_id = "${aws_vpc.moonrake_demo.id}"
+
+  tags = {
+    Name = "moonrake-eks-demo-ig"
+  }
 }
