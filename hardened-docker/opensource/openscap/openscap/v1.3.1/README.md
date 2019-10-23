467efde4fcc63abd5392ba8279055b5d0f3037c6
Kai Prout <kai@moonrake.it>
2019-10-23 17:31:08 -0400

    Adding template for openscap hardened docker image.

---
+++ hardened-docker/opensource/openscap/openscap/v1.3.1/README.md
@@ -0,0 +1,6 @@
+# opensource openscap openscap
+
+TODO Describe this image here
+# v1.3.1  
+
+TODO Describe this image here
---
+++ hardened-docker/opensource/openscap/openscap/v1.3.1/scripts/bootstrap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://github.com/OpenSCAP/openscap/archive/1.3.1.tar.gz --output ../src/openscap-1.3.2.tar.gz
